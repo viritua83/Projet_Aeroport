f7d7884c0c29f2ac5bb4675be9ec477750cf3dd9
isen <Hentwanne@exemple.com>
2023-11-28 11:18:41 +0100

    Ajout affichage

---
+++ affichageMenu.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void afficheTitre() {
+    printf("********/\\************************************************/\\*********\n");
+    printf("*      /  \\                The Two Towers                /  \\       *\n");
+    printf("******/____\\********************************************/____\\*******\n");
+}
+
+void afficheBienvenue() {
+    printf("*     |    |                                            |    |      *\n");
+    printf("*     |    |            Bienvenue dans The Two Towers   |    |      *\n");
+    printf("*     |    |                                            |    |      *\n");
+    printf("*********************************************************************\n");
+}
+
+void afficheOptions() {
+    printf("*  Options disponibles :                                            *\n");
+    printf("*                                                                   *\n");
+    printf("*  1. Commencer la simulation                                       *\n");
+    printf("*  2. Charger des donnees                                           *\n");
+    printf("*  3. Paramètres                                                    *\n");
+    printf("*  4. Quitter                                                       *\n");
+    printf("*                                                                   *\n");
+    printf("*********************************************************************\n");
+}
+
+void afficheMenu() {
+	system("clear");
+    afficheTitre();
+    afficheBienvenue();
+    afficheOptions();
+}
+
+int main() {
+    // Affichage de l'interface
+    afficheMenu();
+    return 0;
+}
