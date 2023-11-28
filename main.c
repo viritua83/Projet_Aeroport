7683c522446585a69e6b8d5283f0526a77a0324c
Scuti-NGC <bouchardlenny83@gmail.com>
2023-11-28 09:11:19 +0100

    Creation structures

--- main.c
+++ main.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+typedef struct Avion Avion;
+
+struct Avion
+{
+	int id;
+	int catégoriePiste; // 0:leger 1:affaire 2:ligne
+	int etat; //0:sol 1:vol
+	int nbPassager;
+};
+
+typedef struct Piste Piste;
+
+struct Piste
+{
+	int numPiste;
+	int longueur;
+	int catégoriePiste; // 0:Petite 1:Moyenne 2:Grande
+	int nbMax;
+	Avion* suiv;
+};
+
+
 int main(int argc, char const *argv[])
 {
 	printf("Y sont ou mes cramptés");
